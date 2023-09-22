5f0ce09645c449697fe0a77cb4603d4577dee3fc
Keishiro Takata <32236070+KT2891@users.noreply.github.com>
2023-09-22 05:48:45 +0000

    [Finish] ShowPage

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,13 +7,15 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to "/top"
+    redirect_to list_path(list.id)
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create'
-  get 'lists/index'
-  get 'lists/show'
+  get 'lists' => 'lists#index'
+  get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/edit'
   get 'top' => "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
