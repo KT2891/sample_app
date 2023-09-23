dac566d39b25871ec623e58e9c4e229cf25abaec
Keishiro Takata <32236070+KT2891@users.noreply.github.com>
2023-09-23 05:31:28 +0000

    [Start] EditPage

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -19,6 +19,13 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'top' => "homes#top"
   get 'lists/new'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/edit'
-  get 'top' => "homes#top"
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
